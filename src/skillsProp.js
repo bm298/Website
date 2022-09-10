08b0f56b5dfbe23e63ae05fc3912d99f7fe1690a
bm298 <musabilal93@gmail.com>
2022-09-10 21:11:28 +0100

    trial

--- src/skillsProp.js
+++ src/skillsProp.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './App.css';
+import cssInSrc from './cssInSrc.png'
+import inspiredAir from './inspiredAir.jpg'
 
 export default function skillsProp(props) {
     
@@ -7,6 +9,10 @@ export default function skillsProp(props) {
         <div className='eachSkill'>
             {/* <img src={`../siteImages/${props.skillPic}`} className="cardSkill"></img> */}
             <img src={`./${props.skillPic}`} className="cardSkill"></img>
+            <img src={`./cssInSrc.png`} className="cardSkill"></img>
+            <img src={cssInSrc} className="cardSkill"></img>
+            <img src={inspiredAir} className="cardSkill"></img>
+            
             <p className="eachSkillName">{props.skillName}</p>
         </div>
     )
